61c66b4dfd531ef58550590fc8cb545aee23eea3
Christian Brown <christian@fly.io>
2023-12-27 22:24:35 -0800

    removed IO.inspects

--- lib/spotify_bot_web/controllers/live/Spotify_live.ex
+++ lib/spotify_bot_web/controllers/live/Spotify_live.ex
@@ -65,7 +65,6 @@ defmodule SpotifyBotWeb.SpotifyLive do
     scope = "user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state streaming"
     state = for _ <- 1..16, into: "", do: <<Enum.random('0123456789abcdef')>>
 
-
     query_params = [
       response_type: "code",
       client_id: System.get_env("CLIENT_ID"),
@@ -78,13 +77,13 @@ defmodule SpotifyBotWeb.SpotifyLive do
     res = HTTPoison.get("#{url}#{query_params}")
 
     # if headers comes back and contains "request_url" or "location" then do a fetch get request and see what the body or res contains
+    # This is returning a status 303 with a redurect_url
     case res do
       {:ok , %{status_code: 200, body: body}} ->
         Logger.info("Auth successful ✅")
         json_data = Jason.decode!(body)
         IO.inspect(json_data)
         {:noreply, socket}
-
 
       {:ok, %{status_code: status_code, body: _body, request_url: request_url}} ->
         Logger.info(status_code: status_code)
@@ -99,7 +98,6 @@ defmodule SpotifyBotWeb.SpotifyLive do
 
 # Authorization Code Flow fetch token
   def handle_event("fetch-token", _params, socket) do
-    # IO.inspect(token_code: socket.assigns.code)
     url = "https://accounts.spotify.com/api/token"
     body = "grant_type=authorization_code&code=#{socket.assigns.code}&redirect_uri=http://localhost:4000"
     headers = [{"Content-Type", "application/x-www-form-urlencoded"}, {"Authorization", "Basic #{Base.encode64("#{System.get_env("CLIENT_ID")}:#{System.get_env("CLIENT_SECRET")}")}"}]
@@ -107,7 +105,7 @@ defmodule SpotifyBotWeb.SpotifyLive do
     res = HTTPoison.post(url, body, headers)
     case res do
       {:ok , %{status_code: 200, body: body}} ->
-        Logger.info("Auth Token Code fetched ✅")
+        Logger.info("Auth Token with Code fetched ✅")
         json_data = Jason.decode!(body)
         {:noreply, assign(socket, access_token: json_data["access_token"], expires_in: json_data["expires_in"], refresh_token: json_data["refresh_token"])}
 
@@ -123,16 +121,11 @@ defmodule SpotifyBotWeb.SpotifyLive do
   end
 
   def handle_event("refresh-token", _params, socket) do
-    IO.inspect(socket.assigns.access_token)
     url = "https://accounts.spotify.com/api/token"
-    # body = "grant_type=refresh_token&refresh_token=#{socket.assigns.refresh_token}&client_id=#{System.get_env("CLIENT_ID")}"
     body = "grant_type=refresh_token&refresh_token=#{socket.assigns.refresh_token}"
     headers = [{"Content-Type", "application/x-www-form-urlencoded"}, {"Authorization", "Basic #{Base.encode64("#{System.get_env("CLIENT_ID")}:#{System.get_env("CLIENT_SECRET")}")}"}]
-    # body = %{grant_type: "refresh_token", refresh_token: refresh_token, client_id: System.get_env("CLIENT_ID")}
 
     res = HTTPoison.post(url, body, headers)
-    # IO.inspect(res)
-
     case res do
       {:ok , %{status_code: 200, body: body}} ->
         Logger.info("Refreshed Token ✅")
@@ -300,6 +293,7 @@ def handle_event("get-player", _params, socket) do
 
     {:ok, %{status_code: status_code, body: body}} ->
       Logger.info('#{status_code}')
+      IO.inspect(body)
       {:noreply, socket}
 
     {:error, error} ->

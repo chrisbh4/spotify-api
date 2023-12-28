95836283959469761ccff34cfce4309b1bcf8c80
Christian Brown <christian@fly.io>
2023-12-27 21:40:23 -0800

    Authentication Flow with redirect_uri is working

--- lib/spotify_bot_web/controllers/live/Spotify_live.ex
+++ lib/spotify_bot_web/controllers/live/Spotify_live.ex
@@ -1,5 +1,4 @@
 defmodule SpotifyBotWeb.SpotifyLive do
-  alias Hex.HTTP
   # alias ElixirLS.LanguageServer.Providers.FoldingRange.Token
   # alias ElixirSense.Log
   use Phoenix.LiveView
@@ -7,10 +6,11 @@ defmodule SpotifyBotWeb.SpotifyLive do
 
   def render(assigns) do
     ~H"""
-
+      <div class='flex justify-center w-full bg-red-500 '>
       <h1>Spotify API access point </h1>
-      <button phx-click="auth">Auth </button>
+      <button phx-click="auth-flow">Auth Redirect </button>
       <button phx-click="fetch-token">Generate Token </button>
+      <button phx-click="refresh-token">Refresh Token </button>
       <button phx-click="get-player">Get Play </button>
       <%!-- <button phx-click="start-timer">Start Timer </button>
       <button phx-click="fetch-artist">Artist data </button>
@@ -18,7 +18,7 @@ defmodule SpotifyBotWeb.SpotifyLive do
       <button phx-click="fetch-playlist"> Demo Playlist</button> --%>
       <button phx-click="play-music">Play Music </button>
       <%!-- <button phx-click="play-song">Play music </button> --%>
-
+      </div>
 
       <%!-- <iframe
         title="DEMO Player "
@@ -31,8 +31,6 @@ defmodule SpotifyBotWeb.SpotifyLive do
         loading="lazy"
       /> --%>
 
-
-
       <%!-- <figure>
         <figcaption>Listen to the T-Rex:</figcaption>
           <audio
@@ -42,28 +40,44 @@ defmodule SpotifyBotWeb.SpotifyLive do
       </figure> --%>
     """
   end
-
-  def mount(_params, _, socket) do
-    {:ok, socket}
-  end
-
-  def handle_event("start-timer", _params, socket) do
-    timer_func()
-    {:noreply, socket}
-  end
-
-
   # https://developer.spotify.com/documentation/web-api/tutorials/code-flow
   # https://community.spotify.com/t5/Spotify-for-Developers/Invalid-username-cant-get-devices/td-p/5193469
   # https://developer.spotify.com/documentation/web-api/reference/get-a-users-available-devices
-  def handle_event("auth", _params, socket) do
-    url = "https://accounts.spotify.com/authorize"
-    # redirect_uri = "https://localhost:4000"
-    scopes = "streaming user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state"
-    body = "grant_type=client_credentials&client_id=#{System.get_env("CLIENT_ID")}&client_secret=#{System.get_env("CLIENT_SECRET")}&scope=#{scopes}"
-    headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
-    res = HTTPoison.post(url, body, headers)
 
+  def mount(params, _, socket) do
+    case params["code"] do
+      nil ->
+        Logger.info("No code")
+        socket = assign(socket, code: nil, state: nil)
+        {:ok, socket}
+
+      _ ->
+        Logger.info("Mounting with code")
+        socket = assign(socket, code: params["code"], state: params["state"])
+        {:ok, socket}
+    end
+  end
+
+
+  def handle_event("auth-flow", _params, socket) do
+    url = "https://accounts.spotify.com/authorize?"
+    redirect_uri = "http://localhost:4000"
+    scope = "user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state streaming"
+    state = for _ <- 1..16, into: "", do: <<Enum.random('0123456789abcdef')>>
+
+
+    query_params = [
+      response_type: "code",
+      client_id: System.get_env("CLIENT_ID"),
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    ]
+    |> URI.encode_query()
+
+    res = HTTPoison.get("#{url}#{query_params}")
+
+    # if headers comes back and contains "request_url" or "location" then do a fetch get request and see what the body or res contains
     case res do
       {:ok , %{status_code: 200, body: body}} ->
         Logger.info("Auth successful ✅")
@@ -71,6 +85,31 @@ defmodule SpotifyBotWeb.SpotifyLive do
         IO.inspect(json_data)
         {:noreply, socket}
 
+
+      {:ok, %{status_code: status_code, body: _body, request_url: request_url}} ->
+        Logger.info(status_code: status_code)
+        Logger.info(request_url)
+        {:noreply, socket}
+
+      {:error, error} ->
+        Logger.info(error)
+        {:noreply, socket}
+      end
+  end
+
+# Authorization Code Flow fetch token
+  def handle_event("fetch-token", _params, socket) do
+    IO.inspect(token_code: socket.assigns.code)
+    url = "https://accounts.spotify.com/api/token"
+    body = "grant_type=authorization_code&code=#{socket.assigns.code}&redirect_uri=http://localhost:4000"
+    headers = [{"Content-Type", "application/x-www-form-urlencoded"}, {"Authorization", "Basic #{Base.encode64("#{System.get_env("CLIENT_ID")}:#{System.get_env("CLIENT_SECRET")}")}"}]
+
+    res = HTTPoison.post(url, body, headers)
+    case res do
+      {:ok , %{status_code: 200, body: body}} ->
+        Logger.info("Auth Token Code fetched ✅")
+        json_data = Jason.decode!(body)
+        {:noreply, assign(socket, access_token: json_data["access_token"], expires_in: json_data["expires_in"], refresh_token: json_data["refresh_token"])}
 
       {:ok, %{status_code: status_code, body: body}} ->
         Logger.info(status_code: status_code)
@@ -83,17 +122,18 @@ defmodule SpotifyBotWeb.SpotifyLive do
       end
   end
 
-  def handle_event("fetch-token", _params, socket) do
+  def handle_event("refresh-token", _params, socket) do
     url = "https://accounts.spotify.com/api/token"
-    scopes = "streaming user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state"
-    body = "grant_type=client_credentials&client_id=#{System.get_env("CLIENT_ID")}&client_secret=#{System.get_env("CLIENT_SECRET")}&scope=#{scopes}"
+    body = "grant_type=refresh_token&refresh_token=#{socket.assigns.refresh_token}&client_id=#{System.get_env("CLIENT_ID")}"
     headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+    # body = %{grant_type: "refresh_token", refresh_token: refresh_token, client_id: System.get_env("CLIENT_ID")}
 
     res = HTTPoison.post(url, body, headers)
     case res do
       {:ok , %{status_code: 200, body: body}} ->
-        Logger.info("Token fetched ✅")
+        Logger.info("Refreshed Token ✅")
         json_data = Jason.decode!(body)
+        IO.inspect(json_data["expires_in"])
         {:noreply, assign(socket, access_token: json_data["access_token"], expires_in: json_data["expires_in"])}
 
       {:ok, %{status_code: status_code, body: body}} ->
@@ -106,6 +146,31 @@ defmodule SpotifyBotWeb.SpotifyLive do
         {:noreply, socket}
       end
   end
+
+# Client Credential Auth Flow
+  # def handle_event("fetch-token", _params, socket) do
+  #   url = "https://accounts.spotify.com/api/token"
+  #   scope = "streaming user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state"
+  #   body = "grant_type=client_credentials&client_id=#{System.get_env("CLIENT_ID")}&client_secret=#{System.get_env("CLIENT_SECRET")}&scope=#{scope}"
+  #   headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+
+  #   res = HTTPoison.post(url, body, headers)
+  #   case res do
+  #     {:ok , %{status_code: 200, body: body}} ->
+  #       Logger.info("Token fetched ✅")
+  #       json_data = Jason.decode!(body)
+  #       {:noreply, assign(socket, access_token: json_data["access_token"], expires_in: json_data["expires_in"])}
+
+  #     {:ok, %{status_code: status_code, body: body}} ->
+  #       Logger.info(status_code: status_code)
+  #       Logger.info(body: body)
+  #       {:noreply, socket}
+
+  #     {:error, error} ->
+  #       Logger.info(error)
+  #       {:noreply, socket}
+  #     end
+  # end
 
   def handle_event("fetch-artist", _params, socket) do
     # Willie
@@ -224,6 +289,7 @@ defmodule SpotifyBotWeb.SpotifyLive do
 def handle_event("get-player", _params, socket) do
   url = "https://api.spotify.com/v1/me/player/devices"
   res = HTTPoison.get(url, [{"Authorization", "Bearer #{socket.assigns.access_token}"}])
+  IO.inspect(res)
   case res do
     {:ok , %{status_code: 200, body: body}} ->
       IO.inspect(body)
@@ -239,6 +305,12 @@ def handle_event("get-player", _params, socket) do
       {:noreply, socket}
   end
 
+  {:noreply, socket}
+end
+
+
+def handle_event("start-timer", _params, socket) do
+  timer_func()
   {:noreply, socket}
 end
 

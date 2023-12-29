4fa14b8a6caec2cc7cac3e67323b6b72c8cd02d6
Christian Brown <christian@fly.io>
2023-12-28 18:14:16 -0800

    Playback State/ be able to stream a song is working

--- lib/spotify_bot_web/controllers/live/Spotify_live.ex
+++ lib/spotify_bot_web/controllers/live/Spotify_live.ex
@@ -8,53 +8,40 @@ defmodule SpotifyBotWeb.SpotifyLive do
     ~H"""
       <div class='flex justify-center w-full bg-red-500 '>
       <h1>Spotify API access point </h1>
-      <button phx-click="auth-flow">Auth Redirect </button>
+      <button phx-click="auth-flow">Auth Flow </button>
       <button phx-click="fetch-token">Generate Token </button>
-      <button phx-click="refresh-token">Refresh Token </button>
-      <button phx-click="get-player">Get Play </button>
+      <%!-- <button phx-click="refresh-token">Refresh Token </button> --%>
+      <button phx-click="get-devices">Get Device </button>
       <%!-- <button phx-click="start-timer">Start Timer </button>
       <button phx-click="fetch-artist">Artist data </button>
       <button phx-click="fetch-top-tracks">Top Tracks </button>
       <button phx-click="fetch-playlist"> Demo Playlist</button> --%>
+      <%!-- <button phx-click="get-currently-playing">Currently Playing </button> --%>
+      <%!-- <button phx-click="fetch-queue">Fetch Queue </button> --%>
       <button phx-click="play-music">Play Music </button>
       <%!-- <button phx-click="play-song">Play music </button> --%>
       </div>
-
-      <%!-- <iframe
-        title="DEMO Player "
-        src={"https://open.spotify.com/embed/playlist/30GBe73yDNLqPBi6fJlUAi?utm_source=generator&theme=0"}
-        width="100%"
-        height="100%"
-        class="h-auto w-1/2"
-        frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      /> --%>
-
-      <%!-- <figure>
-        <figcaption>Listen to the T-Rex:</figcaption>
-          <audio
-              controls
-              src="/media/cc0-audio/t-rex-roar.mp3">
-          </audio>
-      </figure> --%>
     """
   end
   # https://developer.spotify.com/documentation/web-api/tutorials/code-flow
   # https://community.spotify.com/t5/Spotify-for-Developers/Invalid-username-cant-get-devices/td-p/5193469
   # https://developer.spotify.com/documentation/web-api/reference/get-a-users-available-devices
 
-  def mount(params, _, socket) do
+  def mount(_params, _, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
     case params["code"] do
       nil ->
-        Logger.info("No code")
+        Logger.info("Auth :code is nil ❌")
         socket = assign(socket, code: nil, state: nil)
-        {:ok, socket}
+        {:noreply, socket}
 
       _ ->
-        Logger.info("Mounting with code")
+        Logger.info("Auth :code in socket ✅")
         socket = assign(socket, code: params["code"], state: params["state"])
-        {:ok, socket}
+        {:noreply, socket}
     end
   end
 
@@ -62,7 +49,7 @@ defmodule SpotifyBotWeb.SpotifyLive do
   def handle_event("auth-flow", _params, socket) do
     url = "https://accounts.spotify.com/authorize?"
     redirect_uri = "http://localhost:4000"
-    scope = "user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state streaming"
+    scope = "user-read-email user-read-private user-read-playback-state user-read-recently-played user-modify-playback-state streaming user-read-currently-playing"
     state = for _ <- 1..16, into: "", do: <<Enum.random('0123456789abcdef')>>
 
     query_params = [
@@ -96,7 +83,7 @@ defmodule SpotifyBotWeb.SpotifyLive do
       end
   end
 
-# Authorization Code Flow fetch token
+# Authorization Code Flow fetch token // Single Grant token only this is why it is refreshing everytime
   def handle_event("fetch-token", _params, socket) do
     url = "https://accounts.spotify.com/api/token"
     body = "grant_type=authorization_code&code=#{socket.assigns.code}&redirect_uri=http://localhost:4000"
@@ -238,42 +225,32 @@ defmodule SpotifyBotWeb.SpotifyLive do
     end
   end
 
-  # *Try to use HTTPoision libary to be able to stream but I need the 'Album URI' to be able to get the track I want to play
-  #  HTTPoision libary
   def handle_event("play-music", _params, socket) do
-    headers = [
-      {"Authorization", "Bearer #{socket.assigns.access_token}"},
-      {"Content-Type", "application/json"}
-    ]
+    # Macbook App
+    url = "https://api.spotify.com/v1/me/player/play?device_id=9f178b17255f6334556f45148bc1fa3a564ee14e"
+    # Chrome Web Player
+    # url = "https://api.spotify.com/v1/me/player/play?device_id=438a73099346ce1736084c4ba4bc7f01e00a940f"
+    headers = [{"Authorization", "Bearer #{socket.assigns.access_token}"}, {"Content-Type", "application/json"}]
 
+    # offset: is the position of the song in the album in array format starting at 0
     body = '{
       "context_uri": "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
       "offset": {
-          "position": 5
+          "position": 4
       },
       "position_ms": 0
-  }'
-
-#! (CaseClauseError) no case clause matching: %{offset: %{position: 5}, context_uri: "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr", position_ms: 0}
-#   body = %{
-#     context_uri: "spotify:album:5ht7ItJgpBH7W6vJ5BqpPr",
-#     offset: %{
-#         position: 5
-#     },
-#     position_ms: 0
-# }
-
-    url = "https://api.spotify.com/v1/me/player/play"
+    }'
 
     res = HTTPoison.put(url, body, headers)
-
     case res do
-      {:ok , %{status_code: 200, body: body}} ->
-        IO.inspect(body)
+      {:ok , %{status_code: 204}} ->
+        Logger.info("Playback started ✅")
         {:noreply, socket}
 
-      {:ok, %{status_code: status_code}} ->
-        Logger.info('#{status_code}')
+      {:ok, %{status_code: status_code, body: body}} ->
+        Logger.info("Something went wrong ❌")
+        Logger.info(status_code: status_code)
+        IO.inspect(body)
         {:noreply, socket}
 
       {:error, error} ->
@@ -282,10 +259,28 @@ defmodule SpotifyBotWeb.SpotifyLive do
     end
   end
 
-def handle_event("get-player", _params, socket) do
+def handle_event("get-devices", _params, socket) do
   url = "https://api.spotify.com/v1/me/player/devices"
   res = HTTPoison.get(url, [{"Authorization", "Bearer #{socket.assigns.access_token}"}])
-  IO.inspect(res)
+  case res do
+    {:ok , %{status_code: 200, body: body}} ->
+      IO.inspect(Jason.decode!(body))
+      {:noreply, socket}
+
+    {:ok, %{status_code: status_code, body: body}} ->
+      Logger.info(status_code: status_code)
+      IO.inspect(body)
+      {:noreply, socket}
+
+    {:error, error} ->
+      Logger.info(error)
+      {:noreply, socket}
+  end
+end
+
+def handle_event("get-currently-playing", _params, socket) do
+  url = "https://api.spotify.com/v1/me/player/currently-playing"
+  res = HTTPoison.get(url, [{"Authorization", "Bearer #{socket.assigns.access_token}"}])
   case res do
     {:ok , %{status_code: 200, body: body}} ->
       IO.inspect(body)
@@ -300,10 +295,26 @@ def handle_event("get-player", _params, socket) do
       Logger.info(error)
       {:noreply, socket}
   end
-
-  {:noreply, socket}
 end
 
+def handle_event("fetch-queue", _params, socket) do
+  url = "https://api.spotify.com/v1/me/player/queue"
+  res = HTTPoison.get(url, [{"Authorization", "Bearer #{socket.assigns.access_token}"}])
+  case res do
+    {:ok , %{status_code: 200, body: body}} ->
+      IO.inspect(body)
+      {:noreply, socket}
+
+    {:ok, %{status_code: status_code, body: body}} ->
+      Logger.info('#{status_code}')
+      IO.inspect(body)
+      {:noreply, socket}
+
+    {:error, error} ->
+      Logger.info(error)
+      {:noreply, socket}
+  end
+end
 
 def handle_event("start-timer", _params, socket) do
   timer_func()

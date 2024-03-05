547d0f457e9ff0122e4f794f993d24c1629d7afd
Christian Brown <christian@fly.io>
2024-03-05 09:11:57 -0800

    dev.exs updated for both localhost & production

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,13 @@ config :spotify_bot, SpotifyBotWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   # http: [ip: {127, 0, 0, 1}, port: 4000],
+  # _________________________________________________________
+  # Comment back in for Production
   http: [ip: {0, 0, 0, 0}, port: 8080],
+  # _________________________________________________________
+  # Comment back in for Localhost
+  # http: [ip: {0, 0, 0, 0}, port: 4000],
+  # _________________________________________________________
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
